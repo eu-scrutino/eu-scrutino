a3fd40bf475a3bde4ebc60885a1b8d0cde52614e
Nuno Enes <nuno.enes91@gmail.com>
2020-10-13 00:20:34 +0100

    Build app skeleton (Part 1)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'fontsource-roboto';
+import {ThemeProvider} from '@material-ui/core/styles'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import LandingPage from "./pages";
+import theme from './theme'
+
+
+const App = () => (
+    <ThemeProvider theme={theme}>
+      <LandingPage />
+    </ThemeProvider>
+  )
 
 export default App;
